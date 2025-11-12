cd72c415b6cf55b842bcb92af69b582ac526bc5b
afrancop05 <jandro.develop@gmail.com>
2025-11-12 18:39:01 +0100

    Update aplicando filtros de texto y selects

--- DevJobs_React/src/App.jsx
+++ DevJobs_React/src/App.jsx
@@ -12,26 +12,62 @@ const RESULTS_PER_PAGE = 5;
 
 function App() {
 
+	const [filters, setFilters] = useState({
+		technology: "",
+		location: "",
+		experienceLevel: "",
+	});
+	const [textToFilter, setTextToFilter] = useState("");
 	const [currentPage, setCurrentPage] = useState(1);
-	{/* Math.round -> redondea | Math.ceil -> redondea por arriba | Math.floor -> redondea por abajo */}
-	const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE); 
 
-	const pagedResults = jobsData.slice(
+	const jobsFilterByFilters = jobsData.filter((job) => {
+		return (
+			(filters.technology === "" || job.data.technology === filters.technology) &&
+			(filters.location === "" || job.data.modalidad === filters.location) &&
+			(filters.experienceLevel === "" || job.data.nivel === filters.experienceLevel)
+		);
+	})
+
+	const jobsWithFilter = textToFilter === ""
+		? jobsFilterByFilters 
+		: jobsFilterByFilters.filter((job) => {
+			return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
+	})
+	
+	const pagedResults = jobsWithFilter.slice(
 		(currentPage - 1) * RESULTS_PER_PAGE,
 		currentPage * RESULTS_PER_PAGE
 	);
+
+	{/* Math.round -> redondea | Math.ceil -> redondea por arriba | Math.floor -> redondea por abajo */}
+	const totalPages = Math.ceil(jobsWithFilter.length / RESULTS_PER_PAGE); // Se calcula despues de filtrar para que la paginacion sea correcta
+
   const handlePageChange = (page) => {
-    console.log("Página cambiada a:", page);
 		setCurrentPage(page);
   };
   
+	const handleSearch = (filters) => {
+		setFilters(filters);
+		setCurrentPage(1); // Reiniciar a la primera página al cambiar los filtros
+	}
+
+	const handleTextFilter = (newTextToFilter) => {
+		setTextToFilter(newTextToFilter);
+		setCurrentPage(1); // Reiniciar a la primera página al cambiar el filtro de texto
+	};
+
+	
+
 	return (
 		<>
 			{/* Se vuelve a renderizar todo cuando cambia el estado */}
 			<Header />
 
 			<main>
-				<SearchFormSection />
+				<SearchFormSection
+					onSearch={handleSearch}
+					onTextFilter={handleTextFilter}
+				/>
 
 				<section>
 					<JobListings jobs={pagedResults} />
--- DevJobs_React/src/components/SearchFormSection.jsx
+++ DevJobs_React/src/components/SearchFormSection.jsx
@@ -1,10 +1,37 @@
-export function SearchFormSection() {
+import { useId } from "react";
+
+export function SearchFormSection({ onSearch, onTextFilter }) {
+	const idText = useId();
+	const idTechnology = useId();
+	const idLocation = useId();
+	const idExperienceLevel = useId();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		// Esto devuelve un objeto con los datos del formulario
+		const formData = new FormData(event.target);
+
+		const filters = {
+			technology: formData.get(idTechnology),
+			location: formData.get(idLocation),
+			experienceLevel: formData.get(idExperienceLevel),
+		};
+
+		onSearch(filters);
+	};
+
+	const handleTextChange = (event) => {
+		const text = event.target.value.toLowerCase();
+		onTextFilter(text);
+	};
+
 	return (
 		<section className="jobs-search">
 			<h1>Encuentra tu próximo trabajo</h1>
 			<p>Explora miles de oportunidades en el sector tecnológico.</p>
 
-			<form id="empleos-search-form" role="search">
+			<form onSubmit={handleSubmit} id="empleos-search-form" role="search">
 				<div className="search-bar">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -24,17 +51,21 @@ export function SearchFormSection() {
 					</svg>
 
 					<input
-						name="search"
+						name={idText}
 						id="empleos-search-input"
-						required
 						type="text"
 						placeholder="Buscar trabajos, empresas o habilidades"
+						onChange={handleTextChange}
 					/>
+
+					<button type="submit" style={{ position: "absolute", right: "4px" }}>
+						Buscar
+					</button>
 				</div>
 				<span id="search-selected-value"></span>
 
 				<div className="search-filters">
-					<select name="technology" id="filter-technology">
+					<select name={idTechnology} id="filter-technology">
 						<option value="">Tecnología</option>
 						<optgroup label="Tecnologías populares">
 							<option value="javascript">JavaScript</option>
@@ -52,7 +83,7 @@ export function SearchFormSection() {
 						<option value="php">PHP</option>
 					</select>
 
-					<select name="location" id="filter-location">
+					<select name={idLocation} id="filter-location">
 						<option value="">Ubicación</option>
 						<option value="remoto">Remoto</option>
 						<option value="cdmx">Ciudad de México</option>
@@ -61,7 +92,7 @@ export function SearchFormSection() {
 						<option value="barcelona">Barcelona</option>
 					</select>
 
-					<select name="experience-level" id="filter-experience-level">
+					<select name={idExperienceLevel} id="filter-experience-level">
 						<option value="">Nivel de experiencia</option>
 						<option value="junior">Junior</option>
 						<option value="mid">Mid-level</option>
@@ -73,5 +104,5 @@ export function SearchFormSection() {
 
 			<span id="filter-selected-value"></span>
 		</section>
-	)
+	);
 }
