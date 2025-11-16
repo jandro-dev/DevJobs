4d38b158598ef37c1674eba9ea2c08d63d8ee719
afrancop05 <jandro.develop@gmail.com>
2025-11-16 12:25:06 +0100

    Update cargar mensaje si no hay trabajos con los filtros

--- DevJobs_React/src/components/JobListings.jsx
+++ DevJobs_React/src/components/JobListings.jsx
@@ -3,9 +3,14 @@ import { JobCard } from "./JobCard.jsx";
 export function JobListings({ jobs }) {
 	return (
 		<>
-			<h2 style={{ textAlign: "center" }}>Resultados de búsqueda</h2>
-
 			<div className="jobs-listings">
+				{
+					jobs.length === 0 && (
+						<p style={{ textAlign: "center" , padding: "1rem", textWrap: "balance" }}>
+							No se encontraron trabajos que coincidan con los filtros aplicados.
+						</p>
+					)
+				}
 				{jobs.map((job) => (
 					<JobCard key={job.id} job={job} />
 				))}
