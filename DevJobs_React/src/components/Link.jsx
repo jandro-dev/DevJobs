0bdc675b9751c2fa25a38ba5ed269801e09fcd54
afrancop05 <jandro.develop@gmail.com>
2025-11-17 13:15:17 +0100

    Update añadir clase isActive si es la ruta actual con el componente link

--- DevJobs_React/src/components/Link.jsx
+++ DevJobs_React/src/components/Link.jsx
@@ -1,19 +1,25 @@
-import { useRouter } from "../hooks/useRouter"
+import { useRouter } from "../hooks/useRouter";
 
-export function Link({ href, children, ...props}) {
+export function Link({ href, children, className = "", ...props }) {
+	const { currentPath, navigateTo } = useRouter();
 
-	const { navigateTo } = useRouter()
-	
 	const handleClick = (event) => {
+		event.preventDefault();
+		navigateTo(href);
+	};
 
-		event.preventDefault()
-
-		navigateTo(href)
-	}
+	// Determinar si el link está activo
+	const isActive = currentPath === href;
+	const combinedClassName = `${className} ${isActive ? "isActive" : ""}`.trim();
 
 	return (
-		<a href={href} {...props} onClick={handleClick}>
+		<a
+			href={href}
+			{...props}
+			className={combinedClassName}
+			onClick={handleClick}
+		>
 			{children}
 		</a>
-	)
+	);
 }
