26e597e7e06ce6e50e00eb805348e405d60620f8
afrancop05 <jandro.develop@gmail.com>
2025-12-16 12:58:25 +0100

    Add contexto para favoritos

--- DevJobs_React/src/main.jsx
+++ DevJobs_React/src/main.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { FavoritesProvider } from './context/FavContext';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +8,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <App /> {/* Cualquier componente dentro de app puede leer los valores del provider */}
+      <FavoritesProvider>
+        <App /> {/* Cualquier componente dentro de app puede leer los valores del provider */}
+      </FavoritesProvider>
     </AuthProvider>
   </BrowserRouter>,
 )
