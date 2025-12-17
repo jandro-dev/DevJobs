6f9fa55614f03be57c1a1359576131659670b5e5
afrancop05 <jandro.develop@gmail.com>
2025-12-17 13:53:08 +0100

    Add pagina del perfil de usuario ( demo )

--- DevJobs_React/src/App.jsx
+++ DevJobs_React/src/App.jsx
@@ -9,6 +9,7 @@ const HomePage = lazy(() => import("./pages/Home.jsx"))
 const SearchPage = lazy(() => import("./pages/Search.jsx"));
 const NotFoundPage = lazy(() => import("./pages/404.jsx"));
 const JobDetail = lazy(() => import("./pages/Detail.jsx"));
+const ProfilePage = lazy(() => import("./pages/Profile.jsx"));
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
 					<Route path="/" element={<HomePage />} />
 					<Route path="/search" element={<SearchPage />} />
 					<Route path="/jobs/:jobId" element={<JobDetail />} />
+					<Route path="/profile" element={<ProfilePage />} />
 					<Route path="*" element={<NotFoundPage />} />
 				</Routes>
 			</Suspense>
