772b5d1e07663dad6a330e7d30eb8cf092cc0dd4
afrancop05 <jandro.develop@gmail.com>
2026-01-21 12:23:09 +0100

    Update: Escritura de archivo multiplataforma + datos fichero

--- DevJobs_Backend/manage-files.js
+++ DevJobs_Backend/manage-files.js
@@ -1,13 +1,19 @@
 import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { join, basename, extname } from "node:path";
 
 const content = await readFile("./archivo.txt", "utf-8");
 
 console.log(content);
 
-const outputDir = "output/files/documents";
+const outputDir = join("output", "files", "documents");
 
 await mkdir(outputDir, { recursive: true });
 
 const uppercaseContent = content.toUpperCase();
-await writeFile(`./${outputDir}/archivo_mayusculas.txt`, uppercaseContent);
+const outputFilePath = join(outputDir, "archivo_mayusculas.txt");
+
+console.log("La extensión es:", extname(outputFilePath));
+console.log("El nombre del archivo es:", basename(outputFilePath));
+
+await writeFile(outputFilePath, uppercaseContent);
 console.log("Archivo escrito con contenido en mayúsculas.");
