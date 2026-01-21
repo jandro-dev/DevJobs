a39243701454e9da1575400f29d7a43b7782e242
afrancop05 <jandro.develop@gmail.com>
2026-01-21 12:35:10 +0100

    Add: Fichero para ver cosas del sistema

---
+++ DevJobs_Backend/system-info.js
@@ -0,0 +1,12 @@
+import os from "node:os";
+
+console.log("Información del sistema:");
+
+console.log("Tipo de SO:", os.type());
+console.log("Plataforma:", os.platform());
+console.log("Arquitectura:", os.arch());
+console.log("CPU Cores:", os.cpus().length);
+console.log("Total Memory (MB):", Math.round(os.totalmem() / 1024 / 1024));
+console.log("Free Memory (MB):", Math.round(os.freemem() / 1024 / 1024));
+console.log("Home Directory:", os.homedir());
+console.log("Uptime (seconds):", os.uptime());
