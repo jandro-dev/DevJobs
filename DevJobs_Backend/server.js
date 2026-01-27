7c4acc5fd56225d5942f721ba83ba70cc665cc91
afrancop05 <jandro.develop@gmail.com>
2026-01-27 17:27:49 +0100

    Update optimizando el servidor con metodos y filtros

--- DevJobs_Backend/server.js
+++ DevJobs_Backend/server.js
@@ -1,5 +1,6 @@
 import { createServer } from "node:http";
-import { send } from "node:process";
+import { randomUUID } from "node:crypto";
+import { json } from "node:stream/consumers";
 
 process.loadEnvFile(); // lee automáticamente el archivo .env
 const port = process.env.PORT || 3000;
@@ -10,18 +11,64 @@ function sendJson(res, statusCode, data) {
 	res.end(JSON.stringify(data));
 }
 
-const server = createServer((req, res) => {
-	if (req.url === "/") {
-		res.setHeader("Content-Type", "text/plain; charset=utf-8");
-		return res.end("Hola, desde Node! ♥");
+const users = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+	{ id: 3, name: "Charlie" },
+];
+
+const server = createServer(async (req, res) => {
+	const { method, url } = req;
+
+	const [pathName, queryString] = url.split("?");
+
+	const searchParams = new URLSearchParams(queryString);
+
+	if (method === "GET") {
+		if (pathName === "/users") {
+			// Para validar que limit y offset sean números (no es la mejor forma)
+			/* if ( 
+				Number.isNaN(Number(searchParams.get("limit"))) ||
+				Number.isNaN(Number(searchParams.get("offset")))
+			) {
+				return sendJson(res, 400, { error: "Limit and offset must be number" });
+			} */
+
+			const limit = Number(searchParams.get("limit")) || users.length;
+			const offset = Number(searchParams.get("offset")) || 0;
+
+			const paginatedUsers = users.slice(offset, offset + limit);
+
+			return sendJson(res, 200, paginatedUsers);
+		}
+
+		if (pathName === "/health") {
+			return sendJson(res, 200, { status: "OK", uptime: process.uptime() });
+		}
 	}
 
-	if (req.url === "/users") {
-		return sendJson(res, 200, [
-			{ id: 1, name: "Alice" },
-			{ id: 2, name: "Bob" },
-			{ id: 3, name: "Charlie" },
-		]);
+	if (method === "POST") {
+		if (pathName === "/users") {
+			const body = await json(req);
+
+			if (!body || !body.name) {
+				return sendJson(res, 400, { error: "Name is required" });
+			}
+
+			const newUser = {
+				name: body.name,
+				id: randomUUID(),
+			};
+
+			users.push(newUser);
+
+			return sendJson(res, 201, { message: "Usuario creado" });
+		}
+	}
+
+	if (pathName === "/") {
+		res.setHeader("Content-Type", "text/plain; charset=utf-8");
+		return res.end("Hola, desde Node! ♥");
 	}
 
 	return sendJson(res, 404, { error: "Not Found" });
