d639d77fe7ca93f4de2a2db906081fdfca41ac1f
afrancop05 <jandro.develop@gmail.com>
2026-01-28 16:57:13 +0100

    Update usando middlewares antes de las peticiones

--- DevJobs_Backend/Express/index.js
+++ DevJobs_Backend/Express/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 
 const PORT = process.env.PORT ?? 1234;
-
 const app = express();
 
-app.get("/", (request, response) => {
+// Independientemente del metodo que use el cliente, primero pasa por aqui
+app.use((request, response, next) => {
+	const timeString = new Date().toLocaleTimeString();
+	console.log(`[${timeString}] ${request.method} ${request.url}`);
+	next(); // Dejar pasar al siguiente manejador
+});
+
+const previousHomeMiddleware = (request, response, next) => {
+	console.log("Ejecutando el middleware previo a la ruta /");
+	next();
+};
+
+app.get("/", previousHomeMiddleware, (request, response) => {
 	return response.send("<h1>Hello World!</h1>");
 });
 
