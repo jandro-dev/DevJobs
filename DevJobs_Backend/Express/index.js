2eb7ee1a6f17d6a7ac476f47c08a1465143b79f5
afrancop05 <jandro.develop@gmail.com>
2026-01-29 17:07:24 +0100

    Update cargar el json

--- DevJobs_Backend/Express/index.js
+++ DevJobs_Backend/Express/index.js
@@ -1,29 +1,61 @@
 import express from "express";
+import jobs from "./jobs.json" with { type: "json" };
 
 const PORT = process.env.PORT ?? 1234;
 const app = express();
 
 // Independientemente del metodo que use el cliente, primero pasa por aqui
-app.use((request, response, next) => {
+app.use((req, res, next) => {
 	const timeString = new Date().toLocaleTimeString();
-	console.log(`[${timeString}] ${request.method} ${request.url}`);
+	console.log(`[${timeString}] ${req.method} ${req.url}`);
 	next(); // Dejar pasar al siguiente manejador
 });
 
-const previousHomeMiddleware = (request, response, next) => {
-	console.log("Ejecutando el middleware previo a la ruta /");
-	next();
-};
-
-app.get("/", previousHomeMiddleware, (request, response) => {
-	return response.send("<h1>Hello World!</h1>");
+app.get("/", (req, res) => {
+	return res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/health", (request, response) => {
-	return response.json({
+app.get("/health", (req, res) => {
+	return res.json({
 		status: "OK",
 		uptime: process.uptime(),
 	});
+});
+
+// Obtener todos los trabajos
+app.get("/get-jobs", (req, res) => {
+	return res.json(jobs);
+});
+
+// Parametros dinamicos
+app.get("/get-single-job/:id", (req, res) => {
+	const { id } = req.params;
+
+	const idNumber = Number(id);
+
+	return res.json({
+		job: { id: idNumber, title: `Job with id ${idNumber}` },
+	});
+});
+
+// Rutas opcionales con regex -> /acd o /abcd
+app.get("/a{b}cd", (req, res) => {
+	return res.send("/abcd o /acd");
+});
+
+// Comodin
+app.get("/bb*bb", (req, res) => {
+	return res.send("/bb*bb");
+});
+
+// Rutas mas largas que no se saben como terminan
+app.get("/file/*filename", (req, res) => {
+	return res.send("file/*");
+});
+
+// Usando regex
+app.get(/.*fly$/, (req, res) => {
+	return res.send("Terminando en fly");
 });
 
 app.listen(PORT, () => {
